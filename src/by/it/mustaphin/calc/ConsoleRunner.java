cf678ff0fc9d30d8e0305952346afc75b6b2845a
marat223 <pcgeniusweb.mm@gmail.com>
2017-11-20 00:47:23 +0300

    calc.Parser TaskA (без JUnit)

--- src/by/it/mustaphin/calc/ConsoleRunner.java
+++ src/by/it/mustaphin/calc/ConsoleRunner.java
@@ -14,6 +14,9 @@ public class ConsoleRunner {
 //        parser.parseExpression("A=2+5.3");
 //        parser.parseExpression("B=A*3.5");
 //        parser.parseExpression("B1=B+0.11*-5");
-        parser.parseExpression("B2=A/2-1");
+//        parser.parseExpression("B2=A/2-1");
+        parser.parseExpression("C=B+(A*2)");
+        parser.parseExpression("D=((C-0.15)-20)/(7-5)");
+        parser.parseExpression("E={2,3}*(D/2)");
     }
 }
